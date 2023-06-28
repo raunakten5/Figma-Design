aa3d79b16727ff36f97ed9a060f397e8f1941538
raunakten5 <81689161+raunakten5@users.noreply.github.com>
2023-06-28 19:28:22 +0400

    Frame.vue

---
+++ Frame.vue
@@ -0,0 +1,19 @@
+<template>
+  <div :class="$style.frame" />
+</template>
+<script>
+  import { defineComponent, ref } from "vue";
+
+  export default defineComponent({
+    name: "Frame",
+  });
+</script>
+<style module>
+  .frame {
+    position: relative;
+    background-color: var(--color-white);
+    width: 100%;
+    height: 100px;
+    overflow: hidden;
+  }
+</style>
